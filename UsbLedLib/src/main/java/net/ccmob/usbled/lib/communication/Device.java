fe0aeb66e62ff6e37471635f35706c2a7f25ac9a
Marcel Benning <marcel@ccmob.net>
2015-06-17 17:32:00 +0200

    Added stuff

--- UsbLedLib/src/main/java/net/ccmob/usbled/lib/communication/Device.java
+++ UsbLedLib/src/main/java/net/ccmob/usbled/lib/communication/Device.java
@@ -1,45 +1,52 @@
+
 package net.ccmob.usbled.lib.communication;
+
 
 import jssc.SerialPortException;
 import net.ccmob.usbled.lib.dataTypes.Command;
 
+
 public class Device {
 
-    private static Device instance;
-    
-    public static void start(){
-        if(instance == null){
-            setInstance(new Device());
-        }
-    }
-    
-    /**
-     * @return the instance
-     */
-    public static Device getInstance() {
-        if(instance == null){
-            Device.start();
-        }
-        return instance;
-    }
+	private static Device	instance;
+	public static boolean	throwException	= true;
 
-    /**
-     * @param instance the instance to set
-     */
-    public static void setInstance(Device instance) {
-        Device.instance = instance;
-    }
-    
-    public void handleCommand(Command cmd) throws UsbLedException{
-        if(!ControllerInterface.isConnected())
-            throw new UsbLedException("No device connected.", UsbLedState.DEVICE_NOT_CONNECTED);
-        try {
-            ControllerInterface.getInstance().getSerialDevice().writeBytes(cmd.toByteArray());   
-        } catch (SerialPortException e) {
-            if(e.getExceptionType() == SerialPortException.TYPE_PORT_NOT_OPENED){
-                throw new UsbLedException("Device port not opened: " + ControllerInterface.port, UsbLedState.DEVICE_NOT_CONNECTED);
-            }
-        }
-    }
-    
+	public static void start() {
+		if (instance == null) {
+			setInstance(new Device());
+		}
+	}
+
+	/**
+	 * @return the instance
+	 */
+	public static Device getInstance() {
+		if (instance == null) {
+			Device.start();
+		}
+		return instance;
+	}
+
+	/**
+	 * @param instance
+	 *          the instance to set
+	 */
+	public static void setInstance(Device instance) {
+		Device.instance = instance;
+	}
+
+	public void handleCommand(Command cmd) throws UsbLedException {
+		if (!ControllerInterface.isConnected())
+			if (throwException)
+				throw new UsbLedException("No device connected.", UsbLedState.DEVICE_NOT_CONNECTED);
+		try {
+			ControllerInterface.getInstance().getSerialDevice().writeBytes(cmd.toByteArray());
+		} catch (SerialPortException e) {
+			if (e.getExceptionType() == SerialPortException.TYPE_PORT_NOT_OPENED) {
+				if (throwException)
+					throw new UsbLedException("Device port not opened: " + ControllerInterface.port, UsbLedState.DEVICE_NOT_CONNECTED);
+			}
+		}
+	}
+
 }
